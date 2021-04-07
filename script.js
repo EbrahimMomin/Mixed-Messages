2b723f1c102fb39fb69e9aa6a86866153582d346
Ebrahim Momin <80077560+EbrahimMomin@users.noreply.github.com>
2021-04-07 16:37:16 -0700

    Had some bugs before, but not now

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const messages = [
     'Why did the soldier get depressed?\nBecause he has PTSD and his friends died',
     'Why did the chicken cross the road?Because he hates you and wanted to get away from you',
 ];
-const id = Math.floor(Math.random()) * messages.length;
+const id = Math.floor(Math.random() * messages.length);
 const message = messages[id];
 console.log(message);
