f7b4893d055e7c9b6db9e3328eabd8771b6f7203
Ebrahim Momin <80077560+EbrahimMomin@users.noreply.github.com>
2021-04-07 16:28:40 -0700

    Project 100% done. Now. Had to remove some stuff

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const messages = [
     'Why did the scientist go to jail?\nHe murdered 7 people',
     'Why did the soldier get depressed?\nBecause he has PTSD and his friends died',
-    'Why did the zoo keeper crash his Mustang?\nHe was scared it would it eat his pet snake',
     'Why did the chicken cross the road?Because he hates you and wanted to get away from you',
 ];
 const id = Math.floor(Math.random()) * messages.length;
